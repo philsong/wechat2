6d2816f025306ba83691c2f337788bb3f73fefac
chan xuehong <chanxuehong@gmail.com>
2015-01-20 13:28:47 +0800

    no message

--- mp/message/template/msg.go
+++ mp/message/template/msg.go
@@ -5,13 +5,16 @@
 
 package template
 
+import (
+	"encoding/json"
+)
+
 type TemplateMessage struct {
 	ToUser     string `json:"touser"`             // 必须, 接受者OpenID
 	TemplateId string `json:"template_id"`        // 必须, 模版ID
 	URL        string `json:"url,omitempty"`      // 可选, 用户点击后跳转的URL，该URL必须处于开发者在公众平台网站中设置的域中
 	TopColor   string `json:"topcolor,omitempty"` // 可选, 整个消息的颜色, 可以不设置
 
-	// 必须, 模版调用时需要的参数赋值内容，具体有哪些参数视模版内容而定;
-	// Data 一般是一个 *struct 或者 map, 要求被 encoding/json 格式化后满足特定的模板需求.
-	Data interface{} `json:"data"`
+	// 必须, JSON 格式的 []byte, 满足特定的模板需求
+	Data json.RawMessage `json:"data"`
 }
