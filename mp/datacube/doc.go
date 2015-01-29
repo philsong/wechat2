69e99d1b8f624fa1bcf7c507cf1860b1cc790319
chanxuehong <chanxuehong@gmail.com>
2015-01-29 13:20:26 +0800

    增加 数据统计接口

---
+++ mp/datacube/doc.go
@@ -0,0 +1,7 @@
+// @description wechat2 是腾讯微信公众平台 api 的 golang 语言封装
+// @link        https://github.com/chanxuehong/wechat2 for the canonical source repository
+// @license     https://github.com/chanxuehong/wechat2/blob/master/LICENSE
+// @authors     chanxuehong(chanxuehong@gmail.com)
+
+// 数据统计接口.
+package datacube
